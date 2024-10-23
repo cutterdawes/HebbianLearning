db532b05921c4b54c79fc69c6258313f02de39b8
cutterdawes <cutter@dawes.org>
2024-10-23 16:37:23 +0200

    .

--- grid_search.py
+++ grid_search.py
@@ -6,60 +6,10 @@ import training
 import utils
 
 
-# setup logging
-utils.setup_logging(log_file='grid_search.log')
-logging.info('\nSTDP hyperparameter grid...\n')
-
-# hyperparameters to search
-param_grid = {
-    'activation': ['relu', 'tanh'],
-    'beta': [0, 0.1, 0.2, 0.5, 1],
-    'epochs': [1, 3, 5, 10, 20],
-    'lr': [0.0001, 0.0002, 0.0003, 0.0004, 0.0005],
-}
-msg = yaml.dump(param_grid, sort_keys=False, default_flow_style=False)
-logging.info(msg)
-
-# grid search
-logging.info('\nSearching...\n')
-for activation, beta, epochs, lr in itertools.product(
-        param_grid['activation'],
-        param_grid['beta'],
-        param_grid['epochs'],
-        param_grid['lr']
-    ):
-    # create config
-    config = {
-        'model': {
-            'type': 'GenHebb',
-            'input_dim': 784,
-            'hidden_dim': 100,
-            'output_dim': 10,
-            'learning_rule': 'STDP',
-            'activation': activation,
-            'n_hebbian_layers': 1,
-            'importance_factor': 0,
-            'kwargs': {'beta': beta}
-        },
-        'training': {
-            'unsupervised': {
-                'epochs': epochs,
-                'lr': lr,
-                'batch_size': 128
-            },
-            'supervised': {
-                'epochs': 50,
-                'lr': 0.001,
-                'batch_size': 64
-            }
-        }
-    }
-
-    # get device
-    device = utils.get_device()
-
-    # load model
+def run_trial(config):
+    # get model and device
     model = utils.get_model(config['model'])
+    device = utils.get_device()
 
     # unsupervised training
     unsup_trainloader = utils.load_data(config['training'], 'unsupervised', device)
@@ -86,5 +36,73 @@ for activation, beta, epochs, lr in itertools.product(
 
     # evaluate model and log accuracy
     test_acc = training.eval(model, sup_testloader, device)
-    msg = f'{activation}-{beta}_beta-{epochs}_epochs-{lr}_lr: {test_acc:.1f}'
+
+    return test_acc
+
+
+# STDP grid search MNIST
+if __name__ == '__main__':
+
+    # setup logging
+    utils.setup_logging(log_file='grid_search.log')
+
+    # hyperparameters to search
+    logging.info('\nSTDP hyperparameter grid...\n')
+    param_grid = {
+        'activation': ['tanh', 'relu'],
+        'layers': [1, 2, 3],
+        'importance_factor': [0, 10, 100],
+        'beta': [0.1],
+        'epochs': [3],
+        'lr': [0.0001, 0.0002, 0.0003],
+    }
+    msg = yaml.dump(param_grid, sort_keys=False, default_flow_style=False)
     logging.info(msg)
+
+    # grid search
+    logging.info('\nSearching...\n')
+    for activation, layers, importance_factor, beta, epochs, lr in itertools.product(
+            param_grid['activation'],
+            param_grid['layers'],
+            param_grid['importance_factor'],
+            param_grid['beta'],
+            param_grid['epochs'],
+            param_grid['lr']
+        ):
+        # create config
+        config = {
+            'model': {
+                'type': 'GenHebb',
+                'input_dim': 784,
+                'hidden_dim': 100,
+                'output_dim': 10,
+                'learning_rule': 'STDP',
+                'activation': activation,
+                'n_hebbian_layers': layers,
+                'importance_factor': importance_factor,
+                'kwargs': {'beta': beta}
+            },
+            'training': {
+                'unsupervised': {
+                    'epochs': epochs,
+                    'lr': lr,
+                    'batch_size': 128
+                },
+                'supervised': {
+                    'epochs': 50,
+                    'lr': 0.001,
+                    'batch_size': 64
+                }
+            }
+        }
+
+        # run trials
+        num_trials = 1
+        test_acc = 0
+        for _ in range(num_trials):
+            test_acc += run_trial(config)
+        test_acc /= num_trials
+
+        # log accuracy
+        msg = f'{activation}-{layers}_layers-{importance_factor}_importance_factor-{beta}_beta-{epochs}_epochs-{lr}_lr: {test_acc:.1f}'
+        logging.info(msg)
